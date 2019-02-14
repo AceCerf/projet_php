61389ec4f81d63a66aaf24a619920eb701ce7405
AceCerf <cyril.vanloo@gmail.com>
2019-02-14 15:10:10 +0100

    creation de connexion.php + modif de gerard sur extractionDonnees.php

--- class/ExtractionDonnees.php
+++ class/ExtractionDonnees.php
@@ -1,1 +1,35 @@
 <?php
+ class Extraction {
+   private $table;
+   private $find_fic=array();// attribut pour la sélection 
+   
+
+	public function __construct ($find_fic,$table) //constructeur
+   {
+     $this->table = $table;
+     $this->find_fic=$find_fic;
+
+   }
+ 
+  public function postReqSelect($find_fic,$fields) 
+  {
+  
+ // $fields doit être un array
+   $fields = implode(',', $fields);//les différents champs qu'on veut récuperer
+   $sql = $bdd->prepare("SELECT $fields FROM $this->table WHERE nom_fic= ?")
+   $sql = $bdd-> execute(array($_POST['$find_fic']))
+
+ 
+ 
+   // Traitement des données de la requête
+
+   $tab=array();ok
+
+   while ($donnee =$sql->fetch()) 
+   {
+      $tab[] = $donnee['ID_fic'];
+   }
+  return $tab; // Renvoie le tableau associatif avec les données
+ }
+}
+?>
--- connexion.php
+++ connexion.php
@@ -1,2 +1,26 @@
-<?
-//Connexion à la base de donnée
+<?php
+/**
+ * Connexion à la base de donnée mysql
+ * dbname : projet_php
+ * user : root
+ * password : 1234512345
+ */
+try {
+$bdd = new PDO('mysql:host=localhost;dbname=projet_php;charset=utf8', 'root','1234512345');
+}
+catch(Exception $e) {
+    die('Erreur : '.$e->getMessage());
+}
+
+
+/** 
+*test
+*
+*$reponse = $bdd->query('SELECT * FROM `datas` WHERE 1');
+*
+*while ($donnees = $reponse->fetch()){
+*echo $donnees['id']." : ".$donnees['chemin_relatif']." : ".$donnees['description']."<br/>";
+*}
+*
+*$reponse->closeCursor();
+*/
